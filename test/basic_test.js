6fbf7f010c6f477bee73aea0aed275de7ce3035f
Michał Warda <michal.warda@elpassion.pl>
2016-07-20 12:42:02 +0200

    Another one test

--- test/basic_test.js
+++ test/basic_test.js
@@ -9,23 +9,30 @@ VAL
 const otherInput = 
 `16
 yyyy
-VAL
+VAR
 `
 
 function parse(input) {
 	const lines = input.split("\n")
 
-    return {lines: lines[0], indent: lines[1]}
+    return {lines: lines[0], indent: lines[1], output: lines[2]}
 }
 
 describe('parser', () => {
 	it('should parse header of input', () => {
 		assert.deepEqual(
-			parse(input), {lines: 15, indent: "xxxx"}
+			parse(input), {lines: 15, indent: "xxxx", output: "VAL"}
 		);
 		assert.deepEqual(
-			parse(otherInput), {lines: 16, indent: "yyyy"}
+			parse(otherInput), {lines: 16, indent: "yyyy", output: "VAR"}
 		);
 	});
+
+	it('shuould not indent first line', () => {
+		assert.equal(
+			parse(input).output, "VAL"
+		);
+	});
+
 });
 
